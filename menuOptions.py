d6a0542c7c8b6403baafc9459a22ae56047754a2
ISeaFood30 <dannyrnv@gmail.com>
2023-07-10 16:06:10 +0100

    can close and open virtual keyboard

--- menuOptions.py
+++ menuOptions.py
@@ -1,4 +1,6 @@
 import pygame
+import platform
+import pyautogui
 
 
 class menuOptions():
@@ -10,6 +12,7 @@ class menuOptions():
         self.menu_music.set_volume(0.1)
         self.sound = True
         self.camera = True
+        self.keyboard_visible = False
         
         self.menu_click = pygame.mixer.Sound('Sounds/minecraft_click.wav')
         self.menu_click.set_volume(0.1)
@@ -32,4 +35,21 @@ class menuOptions():
     
     def setCamera (self,value,camera):
         self.camera = camera
-    
+        
+    def setKeyboard(self,value, keyboard):
+        self.keyboard_visible = keyboard
+        system = platform.system()
+        
+        if system == 'Windows':
+            pyautogui.hotkey('win', 'ctrl', 'o')
+        elif system == 'Darwin':  # macOS
+            pyautogui.hotkey('command', 'control', 'o')
+        elif system == 'Linux':
+            pyautogui.hotkey('ctrl', 'o')
+        #print(self.keyboard_visible)
+    """
+    def toggleKeyboard(self):
+        if self.keyboard_visible:
+            self.keyboard_visible = False
+        else:
+            self.keyboard_visible = True"""
